d0cd5dfbaf24ff97240fa49ad51a2438726e80db
adityap-si <aditya.pandey@sportzinteractive.net>
2023-08-07 17:09:29 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+  
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Welcome()
+        {
+            return "Hello World";
+        }
+        [HttpGet("display")]
+        public string Display()
+        {
+            return "Hello from Display";
+        }
+    }
+}
